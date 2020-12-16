3095f09ac7e0432be4c7868531d8223fa9023328
Aritz_Garzo <aritz.garzo@alumni.mondragon.edu>
2020-12-16 11:21:51 +0100

    - Añadir imagenes, carpeta de imagenes - Editar codigo (añadir codigo de aplicacion grafica)

---
+++ The_MUltimate_Genius/GureEstrukturak.h
@@ -0,0 +1,16 @@
+#ifndef GUREESTRUKTURAK_H
+#define GUREESTRUKTURAK_H
+
+#include "ebentoak.h"
+
+typedef enum { JOLASTEN, GALDU, IRABAZI }EGOERA;
+typedef enum { IRUDIA, MARGOA, TESTUA } MOTA;
+
+typedef struct
+{
+	POSIZIOA pos;
+	int id;
+	MOTA mota;
+}JOKO_ELEMENTUA;
+
+#endif
